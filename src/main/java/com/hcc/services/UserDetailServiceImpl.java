467185ab45f22c924b573ed1bfd85bae4a455320
ngosi <ngosi.lind@gmail.com>
2024-04-26 15:22:08 -0400

    firstTicket done

--- src/main/java/com/hcc/services/UserDetailServiceImpl.java
+++ src/main/java/com/hcc/services/UserDetailServiceImpl.java
@@ -1,5 +1,27 @@
 package com.hcc.services;
 
+import com.hcc.entities.User;
+import com.hcc.utils.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    CustomPasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
+        return user;
+    }
+}
+
 // import com.hcc.entities.User;
 // import com.hcc.repositories.UserRepository;
 // import com.hcc.utils.CustomPasswordEncoder;
@@ -8,9 +30,9 @@ package com.hcc.services;
 // import org.springframework.security.core.userdetails.UserDetailsService;
 // import org.springframework.security.core.userdetails.UsernameNotFoundException;
 //import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
 
 
 // uncomment this class once you have created all of the needed parts
